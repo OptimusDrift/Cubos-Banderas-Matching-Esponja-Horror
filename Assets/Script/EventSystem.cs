8fbe3121335fb23901bf42ea1930ba159a39f905
Drift (Brandon Urigo) <32204921+OptimusDrift@users.noreply.github.com>
2022-06-13 13:38:58 -0300

    ➕ADD: Moviment

---
+++ Assets/Script/EventSystem.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventSystem : MonoBehaviour
+{
+    public int verde = 0, amarillo = 0, naranja = 0, celeste = 0, rojo = 0, azul = 0, rosa = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public int getColor(string color)
+    {
+        if (color == "Verde")
+        {
+            return verde;
+        }
+        if (color == "Amarillo")
+        {
+            return amarillo;
+        }
+        if (color == "Naranja")
+        {
+            return naranja;
+        }
+        if (color == "Celeste")
+        {
+            return celeste;
+        }
+        if (color == "Rojo")
+        {
+            return rojo;
+        }
+        if (color == "Azul")
+        {
+            return azul;
+        }
+        if (color == "Rosa")
+        {
+            return rosa;
+        }
+        return -1;
+    }
+}
--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
     Rigidbody rb;
     public bool pared;
+    private string movimiento;
+    private Vector2 touchDeltaPosition;
+    private float x, z;
+    public Text debug;
+    public GameObject system;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,22 +22,68 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     private void FixedUpdate()
     {
-        
-        rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, speed);
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+            pared = false;
+        }
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            if (Mathf.Abs(touchDeltaPosition.x) > Mathf.Abs(touchDeltaPosition.y))
+            {
+                if (touchDeltaPosition.x > 0f)
+                {
+                    x = speed;
+                    movimiento = "ParedDerecha";
+                    debug.text = movimiento;
+                }
+                else
+                {
+                    x = -speed;
+                    movimiento = "ParedIzquirda";
+                    debug.text = movimiento;
+                }
+                z = 0;
+            }
+            else
+            {
+                if (touchDeltaPosition.y > 0f)
+                {
+                    z = speed;
+                    movimiento = "ParedArriba";
+                    debug.text = movimiento;
+                }
+                else
+                {
+                    z = -speed;
+                    movimiento = "ParedAbajo";
+                    debug.text = movimiento;
+                }
+                x = 0;
+            }
+        }
+        rb.velocity = new Vector3(x, 0, z);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == gameObject.tag)
+        if (collision.gameObject.tag == movimiento)
         {
-            if (collision.gameObject.GetComponent<PlayerController>().pared)
+            x = 0;
+            z = 0;
+            rb.velocity = Vector3.zero;
+            pared = true;
+        }
+        else
+        {
+            if (collision.gameObject.tag == gameObject.tag && pared)
             {
+                if (system.GetComponent<EventSystem>().getColor(gameObject.tag) <= 2)
+                {
+                    Destroy(collision.gameObject);
+                }
                 Destroy(gameObject);
             }
         }
-        if (collision.gameObject.tag == "Pared"){
-            pared = true;
-        }
-        speed = 0;
     }
 }
